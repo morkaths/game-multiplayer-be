9509bfa59ebcdf70a24510ffc95667b40cd7e572
Morkath <tranduyvuong2004@gmail.com>
2025-07-02 00:16:36 +0700

    fix originn

--- src/server.js
+++ src/server.js
@@ -19,7 +19,6 @@ const io = new Server(server, {
 
 // Middleware
 app.use(cors({
-    origin: process.env.CLIENT_URL,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
